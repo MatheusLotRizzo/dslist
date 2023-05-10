6a0d3093c391ce18222b4f8d761951e1b0f69578
Matheus Lot Rizzo <matheus.l.rizzo@gmail.com>
2023-05-10 12:05:03 -0300

    Estrutura inicial do projeto

---
+++ src/main/java/com/matheusrizzo/dslist/DslistApplication.java
@@ -0,0 +1,13 @@
+package com.matheusrizzo.dslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistApplication {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		SpringApplication.run(DslistApplication.class, args);
+	}
+}
